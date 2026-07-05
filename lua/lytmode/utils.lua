585a3c85368de975bd4125c5bb628ab808e1d226
github-main-user <kolya.skripn@mail.ru>
2026-07-05 17:56:01 +0300

    fix: restore terminal colors broken by palette rename

--- lua/lytmode/utils.lua
+++ lua/lytmode/utils.lua
@@ -2,31 +2,31 @@ local utils = {}
 
 utils.terminal = function(colors)
   -- dark
-  vim.g.terminal_color_0 = colors.vscBack
-  vim.g.terminal_color_8 = colors.vscGray
+  vim.g.terminal_color_0 = colors.lytBack
+  vim.g.terminal_color_8 = colors.lytGray
 
   -- light
-  vim.g.terminal_color_7 = colors.vscFront
-  vim.g.terminal_color_15 = colors.vscFront
+  vim.g.terminal_color_7 = colors.lytFront
+  vim.g.terminal_color_15 = colors.lytFront
 
   -- colors
-  vim.g.terminal_color_1 = colors.vscRed
-  vim.g.terminal_color_9 = colors.vscRed
+  vim.g.terminal_color_1 = colors.lytRed
+  vim.g.terminal_color_9 = colors.lytRed
 
-  vim.g.terminal_color_2 = colors.vscGreen
-  vim.g.terminal_color_10 = colors.vscGreen
+  vim.g.terminal_color_2 = colors.lytGreen
+  vim.g.terminal_color_10 = colors.lytGreen
 
-  vim.g.terminal_color_3 = colors.vscYellow
-  vim.g.terminal_color_11 = colors.vscYellow
+  vim.g.terminal_color_3 = colors.lytYellow
+  vim.g.terminal_color_11 = colors.lytYellow
 
-  vim.g.terminal_color_4 = colors.vscBlue
-  vim.g.terminal_color_12 = colors.vscBlue
+  vim.g.terminal_color_4 = colors.lytBlue
+  vim.g.terminal_color_12 = colors.lytBlue
 
-  vim.g.terminal_color_5 = colors.vscPink
-  vim.g.terminal_color_13 = colors.vscPink
+  vim.g.terminal_color_5 = colors.lytPink
+  vim.g.terminal_color_13 = colors.lytPink
 
-  vim.g.terminal_color_6 = colors.vscBlueGreen
-  vim.g.terminal_color_14 = colors.vscBlueGreen
+  vim.g.terminal_color_6 = colors.lytAccentBlue
+  vim.g.terminal_color_14 = colors.lytAccentBlue
 end
 
 return utils
